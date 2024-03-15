ca854ac11ac89a8627af36b8fc9c30356befc831
vasanth <vasanth@xmedia.in>
2024-03-15 12:50:51 +0530

    chats

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -1,4 +1,5 @@
 import { ChatCircleDots, Phone, Users } from "phosphor-react";
+import { faker } from "@faker-js/faker";
 
 
   const Nav_Buttons = [
@@ -16,4 +17,87 @@ import { ChatCircleDots, Phone, Users } from "phosphor-react";
     },
   ];
 
-  export {Nav_Buttons}
+  const ChatList = [
+    {
+      id: 0,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "9:36",
+      unread: 0,
+      pinned: true,
+      online: true,
+    },
+    {
+      id: 1,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "12:02",
+      unread: 2,
+      pinned: true,
+      online: false,
+    },
+    {
+      id: 2,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "10:35",
+      unread: 3,
+      pinned: false,
+      online: true,
+    },
+    {
+      id: 3,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "04:00",
+      unread: 0,
+      pinned: false,
+      online: true,
+    },
+    {
+      id: 4,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "08:42",
+      unread: 0,
+      pinned: false,
+      online: false,
+    },
+    {
+      id: 5,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "08:42",
+      unread: 0,
+      pinned: false,
+      online: false,
+    },
+    {
+      id: 6,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "08:42",
+      unread: 0,
+      pinned: false,
+      online: false,
+    },
+    {
+      id: 7,
+      img: faker.image.avatar(),
+      name: faker.name.firstName(),
+      msg: faker.music.songName(),
+      time: "08:42",
+      unread: 0,
+      pinned: false,
+      online: false,
+    },
+  ];
+
+  export {Nav_Buttons,ChatList}
--- src/pages/dashboard/Chats.jsx
+++ src/pages/dashboard/Chats.jsx
@@ -1,5 +1,119 @@
-import { Box } from "@mui/material";
+import {
+  Stack,
+  Box,
+  IconButton,
+  Typography,
+  styled,
+  alpha,
+  InputBase,
+  Button,
+  Divider,
+  Badge,
+  Avatar,
+} from "@mui/material";
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
+import { faker } from "@faker-js/faker";
 import React from "react";
+import { ChatList } from "../../data";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+const ChatElement = ({id,img,name,msg,time,unread,online,pinned}) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        borderRadius: 1,
+        backgroundColor: "#fff",
+      }}
+      p={2}
+    >
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Stack direction={"row"} spacing={2}>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant={online ?"dot":"standard"}
+          >
+            <Avatar alt="Remy Sharp" src={img} />
+          </StyledBadge>
+          <Stack spacing={0.3}>
+          <Typography variant="subtitle2">
+            {name}
+          </Typography>
+          <Typography variant="caption">
+            {msg}
+          </Typography>
+        </Stack>
+        </Stack>
+        <Stack spacing={2} alignItems={"center"}>
+          <Typography sx={{fontWeight:600}} variant="caption">{time}</Typography>
+          <Badge color="primary" badgeContent={unread}></Badge>
+        </Stack>
+
+      </Stack>
+    </Box>
+  );
+};
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.background.paper, 1),
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
 
 const Chats = () => {
   return (
@@ -7,11 +121,60 @@ const Chats = () => {
       sx={{
         position: "relative",
         width: 320,
-        height: "100vh",
         backgroundColor: "#F8FAFF",
         boxShadow: "0px 0px 2px rgba(0,0,0,0.2)",
       }}
-    ></Box>
+    >
+      <Stack sx={{height:"100vh"}}>
+      <Stack px={3} pt={3} spacing={2}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Typography variant="h5">Chats</Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
+        </Stack>
+        <Stack sx={{ width: "100%" }}>
+          <Search>
+            <SearchIconWrapper>
+              <MagnifyingGlass color="#709ce6" />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="search..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Stack>
+        <Stack spacing={1}>
+          <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
+            <ArchiveBox size={24} />
+            <Button>Archived</Button>
+          </Stack>
+          <Divider />
+        </Stack>
+
+      </Stack>
+      <Stack p={3} spacing={2} sx={{flexGrow:1,overflowY:"scroll"}}>
+      <Stack direction={"column"} spacing={2}>
+          <Stack spacing={2.4}>
+            <Typography variant="subtitle2" sx={{color:"#676767"}}>Pinned</Typography>
+            {ChatList.filter((i)=>i.pinned).map((ele)=>{
+              return <ChatElement {...ele} key={ele.id}/>
+            })}
+          </Stack>
+          <Stack spacing={2.4}>
+            <Typography variant="subtitle2" sx={{color:"#676767"}}>All Chats</Typography>
+            {ChatList.filter((i)=>!i.pinned).map((ele)=>{
+              return <ChatElement {...ele} key={ele.id}/>
+            })}
+          </Stack>
+        </Stack>
+      </Stack>
+      </Stack>
+    </Box>
   );
 };
 
